f9ee83d497cc7a9906f2b17494ccb960fa1cd05e
root <root@LAPTOP-A2EG90UI.localdomain>
2021-06-21 09:35:42 +0800

    update

--- models.py
+++ models.py
@@ -26,15 +26,15 @@ def Encoder(input_channels = 3, hidden_channels = 256, blk_per_group = 2, group_
   assert hidden_channels >= 64;
   assert blk_per_group >= 1;
   assert vocab_size >= 512;
-  inputs = tf.keras.Input((2**group_num, 2**group_num, input_channels)); # inputs.shape = (batch, height, width, input_channels)
+  inputs = tf.keras.Input((None, None, input_channels)); # inputs.shape = (batch, height, width, input_channels)
   results = tf.keras.layers.Conv2D(hidden_channels, (7,7), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 1/sqrt(input_channels * 7 ** 2)))(inputs); # results.shape = results.shape = (batch, height, width, hidden_channels)
   for i in range(group_num):
     ic = max(2**(i-1) * hidden_channels, hidden_channels);
     oc = 2**i * hidden_channels;
     for j in range(blk_per_group):
       results = EncoderBlock(ic if j == 0 else oc, oc, blk_per_group * group_num)(results);
-      if j != blk_per_group - 1:
-        results = tf.keras.layers.MaxPool2D(pool_size = (2, 2))(results);
+    if i != group_num - 1:
+      results = tf.keras.layers.MaxPool2D(pool_size = (2, 2))(results);
   results = tf.keras.layers.ReLU()(results);
   results = tf.keras.layers.Conv2D(vocab_size, (1,1), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 1/sqrt(2**(group_num - 1) * 1 ** 2)))(results);
   return tf.keras.Model(inputs = inputs, outputs = results);
@@ -44,6 +44,6 @@ if __name__ == "__main__":
   import numpy as np;
   encoder = Encoder(group_num = 4);
   encoder.save('encoder.h5');
-  inputs = np.random.normal(size = (4, 16, 16, 3));
+  inputs = np.random.normal(size = (4, 256, 256, 3));
   outputs = encoder(inputs);
   print(outputs.shape);
