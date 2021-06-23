89822e295d5d840c2bea69538e77e96246120879
root <root@LAPTOP-A2EG90UI.localdomain>
2021-06-24 07:58:14 +0800

    update

--- pretrain.py
+++ pretrain.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+from os.path import exists;
 import tensorflow as tf;
-from models import VQVAE_Encoder, VQVAE_Decoder;
+from models import VQVAE_Trainer;
 
+batch_size = 128;
 
+def main():
+
+  trainer = VQVAE_Trainer();
+  if exists('./checkpoints/chkpt'): trainer.load_weights('./checkpoints/ckpt/variables/variables');
+  optimizer = tf.keras.optimizers.Adam(1e-5);
+  trainer.compile(optimizer = optimizer, loss = {});
