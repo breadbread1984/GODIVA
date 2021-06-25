ba1c3ec25ef3bb0457a5468ab5fab80729e94370
root <root@LAPTOP-A2EG90UI.localdomain>
2021-06-25 23:15:14 +0800

    update

--- save_pretrain_model.py
+++ save_pretrain_model.py
@@ -7,8 +7,6 @@ def main():
 
   trainer = VQVAE_Trainer();
   trainer.load_weights('./checkpoints/ckpt');
-  trainer.encoder.layers[4].set_trainable(False);
-  trainer.encoder.layers[8].set_trainable(False);
   trainer.encoder.save('encoder.h5');
   trainer.decoder.save('decoder.h5');
   
