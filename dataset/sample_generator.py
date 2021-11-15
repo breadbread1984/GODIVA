84d76e10efa27635797f685c1ef215de83e61e3f
breadbread1984 <breadbread1984@163.com>
2021-11-15 15:47:02 +0800

    update

--- dataset/sample_generator.py
+++ dataset/sample_generator.py
@@ -33,12 +33,9 @@ class parse_function(object):
     assert dataset in ['single', 'double'];
     self.encoder = tf.keras.models.load_model(encoder, custom_objects = {'tf': tf, 'Quantize': Quantize, 'QuantizeEma': QuantizeEma});
     self.decoder = tf.keras.models.load_model(decoder, custom_objects = {'tf': tf});
-    top_quantize = self.encoder.layers[4];
-    top_embed_tab = top_quantize.get_embed();
-    top_vocab_size = tf.shape(top_embed_tab)[-1];
-    bottom_quantize = self.encoder.layers[8];
-    bottom_embed_tab = bottom_quantize.get_embed();
-    bottom_vocab_size = tf.shape(bottom_embed_tab)[-1];
+    quantize = self.encoder.layers[4];
+    embed_tab = quantize.get_embed();
+    vocab_size = tf.shape(embed_tab)[-1];
     if dataset == 'single':
       from dataset.mnist_caption_single import dictionary;
       text_vocab_size = len(dictionary);
@@ -47,12 +44,9 @@ class parse_function(object):
       text_vocab_size = len(dictionary);
     self.TEXT_SOS = tf.cast(text_vocab_size, dtype = tf.int64);
     self.TEXT_EOS = tf.cast(text_vocab_size + 1, dtype = tf.int64);
-    self.top_SOS = tf.cast(top_vocab_size, dtype = tf.int64);
-    self.top_EOS = tf.cast(top_vocab_size + 1, dtype = tf.int64);
-    self.bottom_SOS = tf.cast(bottom_vocab_size, dtype = tf.int64);
-    self.bottom_EOS = tf.cast(bottom_vocab_size + 1, dtype = tf.int64);
-    self.top_frame_token_num = img_size // 8 * img_size // 8;
-    self.bottom_frame_token_num = img_size // 4 * img_size // 4;
+    self.VIDEO_SOS = tf.cast(bottom_vocab_size, dtype = tf.int64);
+    self.VIDEO_EOS = tf.cast(bottom_vocab_size + 1, dtype = tf.int64);
+    self.frame_token_num = img_size // 4 * img_size // 4;
   def parse_function(self, sample, text):
     # sample.shape = (length, height, width, channel)
     # text.shape = (length)
@@ -64,13 +58,11 @@ class parse_function(object):
     sample = sample / 255. - 0.5;
     # tile to 3-channel images
     sample = tf.tile(sample, (1,1,1,3)); # samples.shape = (lenght, h, w, 3)
-    # tokens_t.shape = (length, h/8, h/8), tokens_b.shape = (length, h/4, w/4)
-    _, tokens_t, _, _, tokens_b, _ = self.encoder(sample);
-    tokens_t = tf.reshape(tokens_t, (-1,)); # video_token_t.shape = (length * h/8 * w/8)
-    tokens_b = tf.reshape(tokens_b, (-1,)); # video_token_b.shape = (length * h/4 * w/4)
-    outputs_t = tf.concat([tokens_t, tf.ones((self.top_frame_token_num,), dtype = tf.int64) * self.top_EOS], axis = 0); # inputs_t.shape = ((length + 2) * h/8 * w/8)
-    outputs_b = tf.concat([tokens_b, tf.ones((self.bottom_frame_token_num,), dtype = tf.int64) * self.bottom_EOS], axis = 0); # inputs.b.shape = ((length + 2) * h/4 * w/4)
-    return (padded_text, mask), (outputs_t, outputs_b);
+    # tokens.shape = (length, h/4, w/4)
+    _, tokens, _ = self.encoder(sample);
+    tokens = tf.reshape(tokens, (-1,)); # video_token_b.shape = (length * h/4 * w/4)
+    outputs = tf.concat([tokens, tf.ones((self.frame_token_num,), dtype = tf.int64) * self.VIDEO_EOS], axis = 0); # inputs.b.shape = ((length + 2) * h/4 * w/4)
+    return (padded_text, mask), outputs;
 
 if __name__ == "__main__":
 
